6cb5f1fb902a0a8423b3331dc2777104c5d458c9
Shriram Balakrishnan <shriramrbk@gmail.com>
2018-12-09 14:46:31 +0530

    Work In Progress - Try to create all the combinations of letters possible for the digit passed
    Then, use this list to get the common items from the dictionary.txt file contents.

--- word_combinations_generator.rb
+++ word_combinations_generator.rb
@@ -83,4 +83,21 @@ class WordCombinationsGenerator
 
 end
 
+class CombinationGenerator
+  NUMBER_TO_DIGIT_MAPPING = {
+    '2' => ['A', 'B', 'C'],
+    '3' => ['D', 'E', 'F'],
+    '4' => ['G', 'H', 'I'],
+    '5' => ['J', 'K', 'L'],
+    '6' => ['M', 'N', 'O'],
+    '7' => ['P', 'Q', 'R', 'S'],
+    '8' => ['T', 'U', 'V'],
+    '9' => ['W', 'X', 'Y', 'Z']
+  }
+
+  def generate
+    
+  end
+end
+
 p Benchmark.measure { WordCombinationsGenerator.new(6686787825).generate }
