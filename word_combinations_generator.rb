643daa840356a22183c5fd96e9ae44a811903e4e
Shriram Balakrishnan <shriramrbk@gmail.com>
2018-12-09 17:48:12 +0530

    Update DictionaryProcessor class to just fetch words from dictionary.txt file

--- word_combinations_generator.rb
+++ word_combinations_generator.rb
@@ -1,86 +1,16 @@
 require 'pry'
 require 'benchmark'
 
-class WordToNumberConverter
-  attr_reader :word
-
-  def initialize(word)
-    @word = word
-  end
-
-  def convert
-    characters = word.split('')
-    digits = characters.map do |character|
-      DictionaryProcessor::LETTER_DIGIT_MAPPING[character]
-    end
-
-    digits.join
-  end
-end
-
 class DictionaryProcessor
-  attr_reader :words
+  attr_reader :file_location
 
-  LETTER_DIGIT_MAPPING = {
-    'A' => 2,
-    'B' => 2,
-    'C' => 2,
-    'D' => 3,
-    'E' => 3,
-    'F' => 3,
-    'G' => 4,
-    'H' => 4,
-    'I' => 4,
-    'J' => 5,
-    'K' => 5,
-    'L' => 5,
-    'M' => 6,
-    'N' => 6,
-    'O' => 6,
-    'P' => 7,
-    'Q' => 7,
-    'R' => 7,
-    'S' => 7,
-    'T' => 8,
-    'U' => 8,
-    'V' => 8,
-    'W' => 9,
-    'X' => 9,
-    'Y' => 9,
-    'Z' => 9
-  }
-
-  def initialize
-    file_location = 'dictionary.txt'
-    @words = File.read(file_location).split("\n")
+  def initialize(file_location)
+    @file_location = file_location
   end
 
-  def generate_word_digit_mapping
-    word_number_mapping = words.each_with_object( {} ) do |word, hash|
-      hash[word] = WordToNumberConverter.new(word).convert
-    end
-
-    p word_number_mapping.size
+  def read_words
+    File.read(file_location).split("\n")
   end
-end
-
-class WordCombinationsGenerator
-  attr_reader :input_number
-
-  def initialize(input_number)
-    @input_number = input_number
-  end
-
-  def generate
-    get_mapped_dictionary
-  end
-
-  private
-
-  def get_mapped_dictionary
-    DictionaryProcessor.new.generate_word_digit_mapping
-  end
-
 end
 
 class CombinationGenerator
@@ -95,14 +25,18 @@ class CombinationGenerator
     '9' => ['W', 'X', 'Y', 'Z']
   }
 
-  def generate(number)
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def generate
     digits_str_array = number.to_s.split('')
     all_combinations = []
     digits_str_array_size = digits_str_array.size
 
     get_combinations(digits_str_array, all_combinations, 0, "")
-    
-    p all_combinations
   end
 
   def get_combinations(digits_str_array, all_combinations, index_to_process, output_word)
@@ -123,4 +57,8 @@ end
 
 # p Benchmark.measure { WordCombinationsGenerator.new(6686787825).generate }
 
-CombinationGenerator.new.generate(222)
+# p Benchmark.measure { CombinationGenerator.new.generate(6686787825) }
+
+# p Benchmark.measure { DictionaryProcessor.new('dictionary.txt').generate_word_digit_mapping }
+
+words_in_dictionary = DictionaryProcessor.new('dictionary.txt').read_words
