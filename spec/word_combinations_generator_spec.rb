2a4ca6b33a268748ff250a5447a179dd89ad8608
Shriram Balakrishnan <shriramrbk@gmail.com>
2018-12-09 19:49:14 +0530

    Update test case expectation to have upcased values and a missing element
    Elements ["MOTOR"], ["TRUCK"] and ["CAT", "AMOUNTS"] were missed earlier while writing test cases. Included these now.

--- spec/word_combinations_generator_spec.rb
+++ spec/word_combinations_generator_spec.rb
@@ -6,16 +6,16 @@ describe WordCombinationsGenerator do
 
     context 'when initial verification cases are passed' do
 
-      it 'should pass case 1' do
+      it 'should pass basic case 1' do
         input = 6686787825
-        valid_output = [["motor", "usual"], ["noun", "struck"], ["nouns", "truck"], ["nouns", "usual"], ["onto", "struck"], "motortruck"]
+        valid_output = [["MOTOR", "TRUCK"], "MOTORTRUCK", ["MOTOR", "USUAL"], ["NOUN", "STRUCK"], ["NOUNS", "TRUCK"], ["NOUNS", "USUAL"], ["ONTO", "STRUCK"]]
 
         expect(WordCombinationsGenerator.new(input).generate).to eq(valid_output)
       end
 
-      it 'should pass case 2' do
+      it 'should pass basic case 2' do
         input = 2282668687
-        valid_output = [["act", "amounts"], ["act", "contour"], ["acta", "mounts"], ["bat", "amounts"], ["bat", "contour"], ["cat", "contour"], "catamounts"]
+        valid_output = [["ACT", "AMOUNTS"], ["ACTA", "MOUNTS"], ["ACT", "CONTOUR"], ["BAT", "AMOUNTS"], ["BAT", "CONTOUR"], ["CAT", "AMOUNTS"], "CATAMOUNTS", ["CAT", "CONTOUR"]]
 
         expect(WordCombinationsGenerator.new(input).generate).to eq(valid_output)
       end
